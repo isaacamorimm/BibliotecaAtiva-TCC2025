9dc0507a2b1c97b1016ca784ff4ce1b891d2ff4e
isaaczinrs2 <163526904+isaaczinrs2@users.noreply.github.com>
2025-09-02 21:12:15 -0300

    teste

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-// Adicionar na PRIMEIRA LINHA:
 import 'dotenv/config';
-
 import app from "./app.js";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
 
 export default app;
