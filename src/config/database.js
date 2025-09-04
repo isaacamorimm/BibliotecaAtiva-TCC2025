f19e56dda01344d99e7bd1efb73db7e2f7469b53
isaaczinrs2 <163526904+isaaczinrs2@users.noreply.github.com>
2025-09-03 22:43:16 -0300

    a

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,15 @@
 import { Sequelize } from 'sequelize';
+import 'dotenv/config'; 
 
-// Configuração do Sequelize com importação dinâmica do pg
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false,
+  logging: false, 
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  },
-  dialectModule: await import('pg').then(mod => mod.default)
+  }
 });
 
 export default sequelize;
